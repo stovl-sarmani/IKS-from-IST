88e8a790d2b5b0f655924e9192fabbec9f3deba2
srinivasan-chandran <95075559+srinivasan-chandran@users.noreply.github.com>
2021-12-21 15:27:52 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -103,9 +103,9 @@ module "terraform-intersight-iks" {
 # Addon Profile and Policies (To create new change "createNew" to 'true' and uncomment variables and modify them to meet your needs.)
 # This is an Optional item.  Comment or remove to not use.  Multiple addons can be configured.
   addons       = {
-    {
     createNew = false
     use_existing = false
+    # {
     # addonPolicyName = "smm-tf"
     # addonName            = "smm"
     # description       = "SMM Policy"
@@ -122,7 +122,7 @@ module "terraform-intersight-iks" {
     # # installStrategy  = "InstallOnly"
     # releaseVersion = "0.2.61-helm3"
     # # overrides = yamlencode({"demoApplication":{"enabled":true}})
-     }
+    # }
     }
 # Worker Node Instance Type (To create new change "use_existing" to 'false' and uncomment variables and modify them to meet your needs.)
   instance_type = {
